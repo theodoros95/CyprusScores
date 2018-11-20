fe25ba7e3d3f191c73c02b89e92c92f2edeebba5
theodoros95 <theodorosthe@gmail.com>
2018-11-21 01:08:24 +0200

    1

--- app/src/main/java/cs/cyprusscores/SquadActivity.java
+++ app/src/main/java/cs/cyprusscores/SquadActivity.java
@@ -1,10 +1,15 @@
 package cs.cyprusscores;
 
+import android.app.ActionBar;
+import android.app.ListFragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SquadActivity extends AppCompatActivity {
 
@@ -15,21 +20,28 @@ public class SquadActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        //Animation
+        ImageView image1 = findViewById(R.id.imageView2);
+        ImageView image2 = findViewById(R.id.imageView3);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        image1.startAnimation(animation);
+        image2.startAnimation(animation);
     }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item){
         Intent myIntent = new Intent(getApplicationContext(), TeamActivity.class);
         startActivityForResult(myIntent, 0);
         return true;
     }
 
-    public void showPlayer1(View view) {
+    public void showPlayer1(View view){
         Bundle info = new Bundle();
         info.putString("name", "Demetris Christophi");
         info.putString("age", "30");
         info.putString("nat", "Cypriot");
         info.putString("num", "77");
-        info.putString("apps", "20");
+        info.putString("apps", "20" );
         info.putString("goals", "8");
         info.putString("assists", "5");
 
@@ -38,7 +50,7 @@ public class SquadActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void showPlayer2(View view) {
+    public void showPlayer2(View view){
         Bundle info = new Bundle();
         info.putString("name", "Matt Derbyshire");
         info.putString("age", "32");
--- app/src/main/java/cs/cyprusscores/TeamActivity.java
+++ app/src/main/java/cs/cyprusscores/TeamActivity.java
@@ -3,29 +3,40 @@ package cs.cyprusscores;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class TeamActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         WebView webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://en.wikipedia.org/wiki/AC_Omonia");
+
+        //Animation
+        ImageView image = findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+        image.startAnimation(animation);
     }
 
     public void play(View view) {
@@ -33,24 +44,32 @@ public class TeamActivity extends AppCompatActivity {
         Switch s = findViewById(R.id.switch1);
         boolean status = s.isChecked();
 
-        if (status) {
+        if(status)
+        {
             mediaPlayer = MediaPlayer.create(this, R.raw.anthem);
             mediaPlayer.start();
-        } else {
+        }
+        else
+        {
             mediaPlayer.stop();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_team, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
         if (id == R.id.stadium) {
             Intent intentStadium = new Intent(this, StadiumActivity.class);
             startActivity(intentStadium);
